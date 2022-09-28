204a9f99eefc73004433c86a1527e3978dd13925
Davi Galdeano <davi.galdeano@gmail.com>
2022-09-28 16:07:05 -0300

    Projeto python lançado

---
+++ Projeto Dados vazados.py
@@ -0,0 +1,56 @@
+print ("Seja bem vindo ranking de criticidade de dados vazados das empresas")
+
+print('-'* 100)
+
+input('Você deseja saber o ranking de criticidade de dados vazados das empresas?')
+
+sim = '1 lugar de criticidade : Empresa Canva'
+'2 lugar de criticidade : Empresa Hurb'
+'3 lugar de criticidade : Empresa Adobe'
+'4 lugar de criticidade : Empresa Apolo'
+'5 lugar de criticidade : Empresa PDL'
+
+if sim :
+    print('-' * 100)
+    print('RANKING DE CRITICIDADE DE DADOS VAZADOS DAS EMPRESAS:')
+print('1 lugar de criticidade : Empresa Canva')
+print('2 lugar de criticidade : Empresa Hurb')
+print('3 lugar de criticidade : Empresa Adobe')
+print('4 lugar de criticidade : Empresa Apolo')
+print('5 lugar de criticidade : Empresa PDL')
+dados_vazados1 = 'Canva'
+dados_vazados2 = 'Hurb'
+dados_vazados3 = 'Adobe'
+dados_vazados4 = 'Apolo'
+dados_vazados5 = 'PDL'
+
+print('-'* 100)
+
+print('Qual empresa você deseja saber os dados vazados?')
+
+print('Digite Canva para saber seus dados vazados')
+print('Digite Hurb para saber seus dados vazados')
+print('Digite Adobe para saber seus dados vazados')
+print('Digite Apolo para saber seus dados vazados')
+print('Digite PDL para saber seus dados vazados')
+
+print('-'* 100)
+
+input('Digite o nome da empresa que você deseja saber quais foram os dados vazados:')
+
+print('-'* 100)
+
+if dados_vazados1 :
+    print('Os dados vazados foram : endereço de email,localização geográficas,nomes,senhas,nome de usuários.')
+elif dados_vazados2 :
+    print('Os dados vazados foram : data de nascimento,endereço de email,endereço IP,nomes,senhas,némuros de telefones,perfis de mídia social.')
+elif dados_vazados3 :
+    print('Os dados vazados foram : endereço de email,dicas de senhas,senhas,nomede usuário. ')
+elif dados_vazados4 :
+    print('Os dados vazados foram : endereço de email,empregadores,localização geográficas,cargos,nomes,números de telefone,saudações,perfis de mídia social. ')
+elif dados_vazados5 :
+    print('Os dados vazados foram : endereço de email,empregadores,localização geográficas,cargos,nomes,números de telefone,perfis de mídia social.')
+
+
+
+
